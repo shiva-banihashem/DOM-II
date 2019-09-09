c5d784586fb335a5d27f73771c82ddc3cb0a54c4
shiva banihashem <sbanihas@gmail.com>
2019-09-09 18:57:12 -0500

    stretch project part 1 & part 2

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,15 +1,61 @@
 let blocks=document.querySelectorAll('.block');
+let passengers = document.querySelectorAll('.passenger');
 
+let leftvar=0;
+let mousedown=false;
 blocks.forEach((item,i) => {
+    
     item.addEventListener('click', event => {
-     console.log(item);
+     
      classname0 = blocks[0].className;
-    console.log("class name:"+ classname0)
+     passenger0 = passengers[0].innerHTML;
+    
      classnamei= item.className;
+     passengeri = passengers[i].innerHTML;
     blocks[0].setAttribute("class", classnamei);
     blocks[i].setAttribute("class",classname0);
+    passengers[0].innerHTML = passengeri;
+    passengers[i].innerHTML = passenger0;
+    event.stopImmediatePropagation();
+
     
      
     })
+    
+    
+    
   })
-  
+  passengers.forEach ((item,i) => {
+    function movebox(){
+        if(mousedown){
+          item.style.position ='relative';
+          
+          leftarray =item.style.left.split('px');
+          
+          leftvar = Number(leftarray[0]);
+          
+          leftvar = leftvar+100;
+          botvar = item.style.bottom;
+          
+          item.style.left = leftvar +'px';
+          
+          
+          item.style.bottom = botvar;
+         }
+      }
+  item.addEventListener('mousedown', event => {
+       
+    
+
+   
+    mousedown = true;
+     movebox();
+})
+item.addEventListener('mouseup', event => {
+   
+   mousedown = false;
+ 
+
+
+})
+})
